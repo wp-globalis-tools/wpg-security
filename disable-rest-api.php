8f9bba8098e9c443a938cf5ca6e2bd3d12f17d6e
Pierre Dargham <pierre.dargham@globalis-ms.com>
2017-02-07 16:09:51 +0100

    Add plugins headers

--- disable-rest-api.php
+++ disable-rest-api.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * Plugin Name:         WPG Disable Rest API
+ * Plugin URI:          https://github.com/wp-globalis-tools/wpg-security
+ * Description:         Disable Rest API
+ * Author:              Pierre Dargham, Globalis Media Systems
+ * Author URI:          https://www.globalis-ms.com
+ *
+ * Version:             0.1.0
+ * Requires at least:   4.0.0
+ * Tested up to:        4.7.2
+ */
 
 namespace Globalis\Security\DisableRestAPI;
 
